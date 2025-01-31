e78d301e44f7776a618102795b8ce499d0a86c1a
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2025-01-31 22:07:48 +0530

    Basic syntax

--- Javascript_practice/Prac1.js
+++ Javascript_practice/Prac1.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+//const keyword creates a constant
+const account_id = 12345
+let account_email = "hitesh@google.com"
+var account_password = "12345"
+account_city = "jaipur";
+
+/*
+Prefer not to use Var, due to issue in block scope and functional scope
+
+Only use let and const.
+
+we can also declare variable without any type though not recommended if you know the type of the variable
+
+*/
+
+console.log( account_id)
+
+console.table([account_id, account_email, account_password, account_city])
