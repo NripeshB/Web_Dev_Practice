9e7e7903457858efe5101eba7e4f978edb2060ba
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2025-02-04 11:17:44 +0530

    Math and number stuff

--- Javascript_practice/Practice3.js
+++ Javascript_practice/Practice3.js
@@ -1,4 +1,4 @@
-const balance = 100 //acceptable way to define a number
+// const balance = 100 //acceptable way to define a number
 
 // const principal = new Number(1000)//also acceptable;
 
@@ -18,3 +18,31 @@ const balance = 100 //acceptable way to define a number
 // console.log(hundreds.toLocaleString("en-IN"));// gives the Indian numbering standard
 
 
+
+
+console.log(Math.abs(-4)); //gives the absolute value
+
+console.log(Math.round(4.6)); //rounds off the number to the closest non decimal place
+console.log(Math.ceil(4.2)); //returns the top num value to the decimal
+console.log(Math.floor(4.9))// gives the bottom value to the decimal 
+
+//other methods
+//sqrt - square root
+//pow. - power
+//max( num1, num2 ...)
+//min ( num1, num2 ...)
+
+//random
+
+//lets say we have no range then 
+
+console.log(Math.random());
+
+//for integer values below 10
+
+console.log(Math.floor(Math.random()*10 ) + 1 )
+
+//lets say we have a range from 10 to 20 
+
+
+console.log(Math.floor(Math.random()*( 20 - 10 + 1)) + 10 ) 
