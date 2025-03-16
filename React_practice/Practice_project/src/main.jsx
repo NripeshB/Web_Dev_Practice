21646cc950bb2f4b59d9000976e79c3505bd24c4
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2025-03-16 21:29:09 +0530

    routing using react

--- React_practice/Practice_project/src/main.jsx
+++ React_practice/Practice_project/src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Layout from './Layout/Layout.jsx';
+import Home from './components/Home/Home.jsx';
+import About from './components/About/About.jsx';
+import Contact from './components/Contact/Contact.jsx';
 
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>} >
+      <Route path='' element= {<Home/>} />
+      <Route path='About' element= {<About/>} />
+      <Route path='Contact' element= {<Contact/>} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
