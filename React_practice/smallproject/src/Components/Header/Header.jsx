7800670aca69e0dfbe4655314758acbda02f2f41
Nripesh <nripesh.bhusal@gmail.com>
2025-04-22 22:28:39 +0530

    .

--- React_practice/smallproject/src/Components/Header/Header.jsx
+++ React_practice/smallproject/src/Components/Header/Header.jsx
@@ -1,10 +1,12 @@
+import { Container, Logo, LogoutBtn } from '../Index'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 import React from 'react'
 
 function Header(){
-
-return(
-       <div>Header</div>
-      )
+      const authStatus = useSelector((state) => state.auth.status)
+      
 }
 
 export default Header
--- React_practice/smallproject/src/Components/Index.js
+++ React_practice/smallproject/src/Components/Index.js
@@ -1,6 +1,12 @@
 
 import Footer from "./Footer/Footer.jsx";
 import Header from "./Header/Header.jsx";
-
-
-export {Header, Footer}
+import Logo from ".Logo,jsx"
+import Container from "./Container/Container.jsx"
+import LogoutBtn from "./Header/LogoutBtn.jsx"
+export {
+Header, 
+Footer,
+Logo,
+Container,
+LogoutBtn}
