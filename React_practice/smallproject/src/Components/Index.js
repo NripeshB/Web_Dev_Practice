f660dbd2dcdbd7b2cf46af7924bb73b9019e9d50
Nripesh <nripesh.bhusal@gmail.com>
2025-05-29 09:54:51 +0530

    post form

--- React_practice/smallproject/src/Components/Index.js
+++ React_practice/smallproject/src/Components/Index.js
@@ -4,10 +4,12 @@ import Header from "./Header/Header.jsx";
 import Logo from ".Logo,jsx"
 import Container from "./Container/Container.jsx"
 import LogoutBtn from "./Header/LogoutBtn.jsx"
+import RTE from "./RTE.jsx";
 
 export {
 Header, 
 Footer,
 Logo,
 Container,
+RTE,
 LogoutBtn}
